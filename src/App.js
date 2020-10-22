70ee8a0b187965f13898c3aee3a6453f86b340ae
Gokulakrishnan <gokulakrishnanmay29@gmail.com>
2020-10-22 14:22:51 +0530

    menu item completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from "./component/homepage";
+import HomePage from "./pages/homepage/HomePage";
 
 function App() {
     return (
---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import './homepage.scss'
+import Directory from "../../component/directory/Directory";
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div className={'homepage'}>
+                <Directory/>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
