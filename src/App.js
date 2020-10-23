63c84fa6a723356f118ac1f8d2f8c8a49f3444cf
Gokulakrishnan <gokulakrishnanmay29@gmail.com>
2020-10-23 15:07:11 +0530

    Navigation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import {Route, Switch} from 'react-router-dom';
 import HomePage from "./pages/home-page/HomePage";
 import './App.css';
 import ShopPage from "./pages/shop-page/ShopPage";
+import Header from "./component/header/Header";
 
 function App() {
     return (
         <div>
+            <Header/>
             <Switch>
                 <Route exact path={'/'} component={HomePage}/>
                 <Route path={'/shop'} component={ShopPage}/>
